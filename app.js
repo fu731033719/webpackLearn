e37280b11ae29165580415561502e0bc43f8b896
fuchenwei <fuchenwei@citytogo.com>
2018-12-03 17:56:53 +0800

    es_6 pack

---
+++ app.js
@@ -0,0 +1,7 @@
+let func = () => {
+
+}
+const NUM = 45
+let arr = [1, 2, 4]
+let arrB = arr.map(item => item * 2)
+console.log('arrB:', arrB)
---
+++ basic_pack/app.js
@@ -0,0 +1,4 @@
+import sum from './sum'
+var minus = require('./minus')
+console.log('minus(23, 24) = ', minus(23, 24))
+console.log('sum(23, 24) = ', sum(23, 24))
